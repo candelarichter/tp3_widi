60256a336f9965a237e9587a6ff4f45e84e1fc2c
“Candela” <candelarichter.epet20@gmail.com>
2022-04-24 19:44:03 -0300

    first commit

---
+++ java.js
@@ -0,0 +1,6 @@
+var principal = document.getElementById('principal')
+const Etiquetah1 = document.createElement('h1')
+
+console.log(Etiquetah1);
+Etiquetah1.innerHTML = "HOLA"
+principal.appendChild(Etiquetah1)
